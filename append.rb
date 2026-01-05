afe5a91b40b587e74af9c0b06a5bebed81ac5995
SupeChicken666 <me@supechicken666.dev>
2026-01-05 10:52:08 +0000

    Initial commit
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

---
+++ append.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'digest/sha2'
+
+ASB_LEVEL = ARGV[0]
+
+%w[system vendor].each do |img|
+  %w[arm64 x86_64].each do |arch|
+    %w[GAPPS VANILLA MAINLINE].each do |type|
+      next unless File.exist?("#{img}/waydroid_tv_#{arch}/#{type}.json")
+
+      json = JSON.load_file("#{img}/waydroid_tv_#{arch}/#{type}.json", symbolize_names: true)
+
+      Dir["lineage-*-#{type}-waydroid_tv_#{arch}-#{img}.zip"].each do |zip|
+        stat = File.stat(zip)
+
+        json[:response] << {
+          datetime: stat.mtime.to_i,
+          filename: zip,
+          id:       Digest::SHA256.file(zip),
+          romtype:  type,
+          asb:      ASB_LEVEL,
+          size:     stat.size,
+          url:      "https://sourceforge.net/projects/waydroid/files/images/#{img}/waydroid_tv_#{arch}/#{zip}/download",
+          version:  zip[/^lineage-(.+?)-/, 1]
+        }
+      end
+
+      json[:response].sort_by! { |e| - e[:datetime] }
+
+      File.write("#{img}/waydroid_tv_#{arch}/#{type}.json", JSON.pretty_generate(json))
+    end
+  end
+end
---
+++ index.js
@@ -0,0 +1,35 @@
+const AVAILABLE_IMG_TYPES = [
+  ['system', 'arm64', 'GAPPS'],
+  ['system', 'x86_64', 'GAPPS'],
+
+  ['system', 'arm64', 'VANILLA'],
+  ['system', 'x86_64', 'VANILLA'],
+
+  ['vendor', 'arm64', 'MAINLINE'],
+  ['vendor', 'x86_64', 'MAINLINE']
+];
+
+async function getLatestRelease(img, arch, type) {
+  const update_channel = await fetch(`/${img}/waydroid_tv_${arch}/${type}.json`).then(r => r.json());
+  return update_channel.response[0];
+}
+
+async function updateTable(img, arch, type) {
+  const table          = document.getElementById(`${img}-images`),
+        img_info       = await getLatestRelease(img, arch, type),
+        img_table_elem = document.createElement('tr');
+
+  img_table_elem.innerHTML = `
+    <td>${arch}</td>
+    <td>${type}</td>
+    <td>${new Date(img_info.datetime * 1000).toISOString().split('T')[0]}</td>
+    <td>${img_info.asb}</td>
+    <td><a class="button" href="${img_info.url}">SourceForge</a></td>
+  `;
+
+  table.appendChild(img_table_elem);
+}
+
+(async () => {
+  for (types of AVAILABLE_IMG_TYPES) await updateTable(...types);
+})();
